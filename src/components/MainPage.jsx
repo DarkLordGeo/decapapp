5d27ec9de08a184cea511692e273df54a2fc0e27
DarkLordGeo <148902362+DarkLordGeo@users.noreply.github.com>
2025-09-17 21:31:09 +0400

    test

---
+++ src/components/MainPage.jsx
@@ -0,0 +1,29 @@
+import Navbar from "./Navbar"
+
+function MainPage() {
+    return (
+        <>
+            <div className="h-[800px] bg-[url(https://eigensinnig-wien.com/cdn/shop/files/all-black-women-outfits-eigensinnig-wien.webp?v=1738449239)] font-sans">
+                <div className='absolute inset-0 h-[800px] bg-black/50 backdrop-blur-[4px]'></div>
+                <div className='relative z-10 h-full flex justify-between flex-col items-center'>
+                    <Navbar />
+                    <div className="w-full text-center py-32">
+                        <h1 className="text-4xl text-white">Garter</h1>
+                    </div>
+                    <h1 className="text-[4rem] text-white">Your confidence starts here</h1>
+                    <div className="w-full py-[125px] ">
+                        <ul className="flex w-full grow-1">
+                            <li className="text-center text-white bg-black/70 font-thin w-full !py-10 text-3xl hover:scale-95 transition-all cursor-pointer">Bed linen</li>
+                            <li className="text-center text-white bg-black/70 font-thin w-full !py-10 text-3xl hover:scale-95 transition-all cursor-pointer">Swimsuits</li>
+                            <li className="text-center text-white bg-black/70 font-thin w-full !py-10 text-3xl hover:scale-95 transition-all cursor-pointer">Gifts</li>
+                            <li className="text-center text-white bg-black/70 font-thin w-full !py-10 text-3xl hover:scale-95 transition-all cursor-pointer">So on</li>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default MainPage
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+
+
+function Navbar() {
+    return (
+        <>
+            <nav className="flex flex-row items-center justify-center gap-4 text-xl w-2/3 rounded-xl absolute top-[100px] left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <ul className="flex flex-row justify-between py-12 w-full bg-gray-300/10 text-white rounded-2xl">
+                    <li className="font-bold !py-5 !w-full text-center border-r-2  hover:bg-white/50  transition-all duration-95 cursor-pointer">
+                        მთავარი
+                    </li>
+                    <li className="font-bold !py-5 !w-full text-center border-r-2  hover:bg-white/50 transition-all duration-95 cursor-pointer">
+                        კატალოგი
+                    </li>
+                    <li className="font-bold !py-5 !w-full text-center border-r-2  hover:bg-white/50 transition-all duration-95 cursor-pointer">
+                        ჩვენ შესახებ
+                    </li>
+                    <li className="font-bold !py-5 !w-full text-center border-r-2  hover:bg-white/50 transition-all duration-95 cursor-pointer">
+                        კონტაქტი
+                    </li>
+                    <li className="font-bold !py-5 !w-full text-center border-r-2 hover:bg-white/50 transition-all duration-95 cursor-pointer">
+                        FAQ
+                    </li>
+                    <li className="font-bold !py-5 !w-full text-center  hover:bg-white/50 transition-all duration-95 cursor-pointer">
+                        სურვილების სია
+                    </li>
+                </ul>
+            </nav>
+        </>
+    )
+
+}
+
+export default Navbar
