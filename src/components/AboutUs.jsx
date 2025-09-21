6deb8ff06a06db6b7a391e9d2f830742435e65d0
DarkLordGeo <148902362+DarkLordGeo@users.noreply.github.com>
2025-09-21 19:09:34 +0400

    test for about page

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -19,8 +19,8 @@ function AboutUs() {
         <>
             <BackgroundSection height="auto" >
                 <div className='relative w-4/5  min-h-[500px] lg:w-2/5 bg-white !px-6 !pt-10 !pb-8 !mt-72 !mb-32 sm:mx-auto rounded-lg'>
-                    <h1 className='font-bold text-2xl lg:text-5xl !mb-5 lg:!mb-12 text-center'>{cmsData.about_page_header}</h1>
-                    <p className='text-base  w-auto'>{cmsData.about_page_description}</p>
+                    <h1 className='font-bold text-2xl lg:text-5xl !mb-5 lg:!mb-12 text-center'>{cmsData?.about_page_header || 'something test'}</h1>
+                    <p className='text-base  w-auto'>{cmsData?.about_page_description || <p>About us description</p>}</p>
                 </div>
             </BackgroundSection>
         </>
