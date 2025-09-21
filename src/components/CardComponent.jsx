f12dff3559b5f4a40fa0033f2210455635e11393
DarkLordGeo <148902362+DarkLordGeo@users.noreply.github.com>
2025-09-21 22:40:43 +0400

    test

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -1,9 +1,49 @@
+import products from '/src/content/products/products.json'
+
 
 
 function CardComponent() {
+
+    const asd = [
+        {
+            "message_text": "Exercitation asperio",
+            "product_image": "https://americantall.com/cdn/shop/products/American-Tall-Men-Essential-SLIM-FIT-Crew-Neck-Tees-White-front.jpg?v=1673367625",
+            "product_name": "Nadine House",
+            "product_sizes": [
+                "Corporis eiusmod aut",
+                "Similique eius nisi ",
+                "Vel voluptate assume"
+            ],
+            "product_price": "292",
+            "product_available": true
+        }
+    ]
+
+    console.log(products);
+
     return (
         <>
-            <div className="flex w-5/6 h-full flex-col items-center justify-center bg-white">
+            {products.product_list?.map((item, key) => (
+                <div className="flex w-5/6 h-full flex-col items-center justify-center bg-white">
+                    <a className='overflow-hidden cursor-pointer w-full flex items-center justify-center'>
+                        <img
+                            className="h-full aspect-[2/3] object-cover  hover:scale-105 transition-all duration-200"
+                            src={item.product_image}
+                            alt={item.message_text} />
+                    </a>
+                    <div className="mt-4 pb-5">
+                        <a>
+                            <h5 className="text-center tracking-tight text-gray-500">{item.product_name}</h5>
+                        </a>
+                        <div className=" flex justify-center">
+                            <p>
+                                <span className="text-sm font-bold text-gray-900">{item.product_price}</span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            ))}
+            {/* <div className="flex w-5/6 h-full flex-col items-center justify-center bg-white">
                 <a className='overflow-hidden cursor-pointer w-full flex items-center justify-center'>
                     <img
                         className="h-full object-cover aspect-square hover:scale-105 transition-all duration-200"
@@ -110,7 +150,7 @@ function CardComponent() {
                         </p>
                     </div>
                 </div>
-            </div>
+            </div> */}
         </>
     )
 }
